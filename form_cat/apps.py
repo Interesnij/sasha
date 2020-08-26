3a744b9633944de7d96a3ca17d5e2fa74bf9d954
root <root@151-248-122-162.ovz.vps.regruhosting.ru>
2020-08-26 15:26:01 +0000

    J

---
+++ form_cat/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class FormCatConfig(AppConfig):
+    name = 'form_cat'
---
+++ proects/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ProectsConfig(AppConfig):
+    name = 'proects'

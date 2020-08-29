37d5407053279a4575e35281b2e80451ccf8c50e
Serg <ochkarik1983@mail.ru@>
2020-08-29 15:14:21 +0300

    H

--- proects/models.py
+++ proects/models.py
@@ -7,7 +7,7 @@ from imagekit.models import ProcessedImageField
 
 class Proect(models.Model):
 	title = models.CharField(max_length=200, verbose_name="Название")
-    slug = models.CharField(max_length=200, unique=True, verbose_name="Английский аналог для вставки в ссылку")
+	slug = models.CharField(max_length=200, unique=True, verbose_name="Английский аналог для вставки в ссылку")
 	description = RichTextUploadingField(config_name='default', external_plugin_resources=[('youtube','/static/ckeditor_plugins/youtube/youtube/','plugin.js',)],)
 	created = models.DateTimeField(auto_now_add=True, auto_now=False, verbose_name="Создан")
 	image = ProcessedImageField(format='JPEG', options={'quality': 90}, upload_to="proects/proects", processors=[ResizeToFit(width=1024, upscale=False)])
@@ -22,5 +22,5 @@ class Proect(models.Model):
         return self.title
 
 	@classmethod
-    def get_next_proect(cls, self): 
+    def get_next_proect(cls, self):
 		return cls.objects.filter(pk__gt=self.pk).order_by('pk').first()

c1558926e84b0ed2a6283f3a751a22e2c539ee1e
Serg <ochkarik1983@mail.ru@>
2020-08-29 15:16:44 +0300

    H

--- proects/views.py
+++ proects/views.py
@@ -6,20 +6,20 @@ from proects.models import Proect
 
 class ProectsListView(ListView, CategoryListMixin):
 	model = Proect
-    template_name="proects_index.html"
+	template_name="proects_index.html"
 
 	def get_queryset(self):
 		proects = Proect.objects.only("pk")
 		return proects
 
 class ProectView(TemplateView):
-    template_name = "proect.html"
+	template_name = "proect.html"
 
-    def get(self,request,*args,**kwargs):
-        self.proect = Proect.objects.get(slug=self.kwargs["slug"])
-        return super(ProectView,self).get(request,*args,**kwargs)
+	def get(self,request,*args,**kwargs):
+		self.proect = Proect.objects.get(slug=self.kwargs["slug"])
+		return super(ProectView,self).get(request,*args,**kwargs)
 
-    def get_context_data(self,**kwargs):
-        context=super(ProectView,self).get_context_data(**kwargs)
-        context["object"] = self.proect
-        return context
+	def get_context_data(self,**kwargs):
+		context=super(ProectView,self).get_context_data(**kwargs)
+		context["object"] = self.proect
+		return context

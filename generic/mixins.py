c713686fe76c947342e2418ac28ef5d4bb6a5616
Serg <ochkarik1983@mail.ru@>
2020-08-29 20:35:22 +0300

    H

--- generic/mixins.py
+++ generic/mixins.py
@@ -1,10 +1,12 @@
 from django.views.generic.base import ContextMixin
 from django.conf import settings
+from movie_cat.models import MovieCategory
 
 
 class CategoryListMixin(ContextMixin):
 
 	def get_context_data(self,**kwargs):
-		context=super(CategoryListMixin,self).get_context_data(**kwargs)
-		context["current_url"]=self.request.path
+		context = super(CategoryListMixin,self).get_context_data(**kwargs)
+		context["current_url"] = self.request.path
+		context["movie_categories"] = MovieCategory.objects.only("pk")
 		return context
--- movie_cat/models.py
+++ movie_cat/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class MovieCategory(models.Model):
+	name = models.CharField(max_length=100,verbose_name="Название")
+	order = models.PositiveSmallIntegerField(default=0,verbose_name="Порядковый номер")
+	def __str__(self):
+		return self.name
+	class Meta:
+		ordering = ["order","name"]
+		verbose_name = "категория видео"
+		verbose_name_plural = "категории видео"
+
+	def __str__(self):
+		return self.name

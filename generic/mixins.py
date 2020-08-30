108df0f39049c550b7ccc191aa60d227ce718d17
Serg <ochkarik1983@mail.ru@>
2020-08-30 15:19:49 +0300

    H

--- generic/mixins.py
+++ generic/mixins.py
@@ -1,6 +1,6 @@
 from django.views.generic.base import ContextMixin
 from django.conf import settings
-from movie_cat.models import MovieCategory
+from movie_cat.models import VideoCategory
 
 
 class CategoryListMixin(ContextMixin):
@@ -8,5 +8,5 @@ class CategoryListMixin(ContextMixin):
 	def get_context_data(self,**kwargs):
 		context = super(CategoryListMixin,self).get_context_data(**kwargs)
 		context["current_url"] = self.request.path
-		context["movie_categories"] = MovieCategory.objects.only("pk")
+		context["movie_categories"] = VideoCategory.objects.only("pk")
 		return context

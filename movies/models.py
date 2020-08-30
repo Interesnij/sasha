0bab7fb0ad5b90000b8c455b66743d4a17ec0700
Serg <ochkarik1983@mail.ru@>
2020-08-30 15:19:14 +0300

    H

--- movies/models.py
+++ movies/models.py
@@ -31,3 +31,94 @@ class Video(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class VideoComment(models.Model):
+    parent_comment = models.ForeignKey('self', on_delete=models.CASCADE, related_name='video_comment_replies', null=True, blank=True, verbose_name="Родительский комментарий")
+    created = models.DateTimeField(auto_now_add=True, auto_now=False, verbose_name="Создан")
+    modified = models.DateTimeField(auto_now_add=True, auto_now=False, db_index=False)
+    commenter = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name="Комментатор")
+    text = models.TextField(blank=True)
+    is_edited = models.BooleanField(default=False, verbose_name="Изменено")
+    is_deleted = models.BooleanField(default=False, verbose_name="Удаено")
+    video_comment = models.ForeignKey(Video, on_delete=models.CASCADE, blank=True)
+    id = models.BigAutoField(primary_key=True)
+
+    class Meta:
+        indexes = (BrinIndex(fields=['created']), )
+        verbose_name = "комментарий к ролику"
+        verbose_name_plural = "комментарии к ролику"
+
+    def __str__(self):
+        return "{0}/{1}".format(self.commenter.get_full_name(), self.text[:10])
+
+    def get_created(self):
+        from django.contrib.humanize.templatetags.humanize import naturaltime
+        return naturaltime(self.created)
+
+    def get_replies(self):
+        get_comments = VideoComment.objects.filter(parent_comment=self).all()
+        return get_comments
+
+    def count_replies(self):
+        return self.video_comment_replies.count()
+
+    def likes(self):
+        likes = VideoCommentVotes.objects.filter(item=self, vote__gt=0)
+        return likes
+
+    def likes_count(self):
+        likes = VideoVotes.objects.filter(parent=self, vote__gt=0).values("pk")
+        return likes.count()
+
+    def dislikes_count(self):
+        dislikes = VideoVotes.objects.filter(parent=self, vote__lt=0).values("pk")
+        return dislikes.count()
+
+    def dislikes(self):
+        dislikes = VideoCommentVotes.objects.filter(item=self, vote__lt=0)
+        return dislikes
+
+    def likes_count(self):
+        likes = VideoCommentVotes.objects.filter(item=self, vote__gt=0).values("pk")
+        return likes.count()
+
+    def dislikes_count(self):
+        dislikes = VideoCommentVotes.objects.filter(item=self, vote__lt=0).values("pk")
+        return dislikes.count()
+
+    @classmethod
+    def create_comment(cls, commenter, video_comment=None, parent_comment=None, text=None, created=None ):
+        comment = VideoComment.objects.create(commenter=commenter, parent_comment=parent_comment, video_comment=video_comment, text=text)
+        comment.save()
+        return comment
+
+    def count_replies_ru(self):
+        count = self.video_comment_replies.count()
+        a = count % 10
+        b = count % 100
+        if (a == 1) and (b != 11):
+            return str(count) + " ответ"
+        elif (a >= 2) and (a <= 4) and ((b < 10) or (b >= 20)):
+            return str(count) + " ответа"
+        else:
+            return str(count) + " ответов"
+
+
+class VideoVotes(models.Model):
+    LIKE = 1
+    DISLIKE = -1
+    VOTES = ((DISLIKE, 'Не нравится'),(LIKE, 'Нравится'))
+
+    vote = models.IntegerField(default=0, verbose_name="Голос", choices=VOTES)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name="Пользователь")
+    parent = models.ForeignKey(Video, on_delete=models.CASCADE)
+
+class VideoCommentVotes(models.Model):
+    LIKE = 1
+    DISLIKE = -1
+    VOTES = ((DISLIKE, 'Не нравится'),(LIKE, 'Нравится'))
+
+    vote = models.IntegerField(verbose_name="Голос", choices=VOTES)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name="Пользователь")
+    item = models.ForeignKey(VideoComment, on_delete=models.CASCADE)
--- users/models.py
+++ users/models.py
@@ -1,3 +1,17 @@
+from django.utils import timezone
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.conf import settings
+from django.db.models import Q
+from rest_framework.exceptions import PermissionDenied
 
-# Create your models here.
+
+class User(AbstractUser):
+    last_activity = models.DateTimeField(default=timezone.now, blank=True, verbose_name='Активность')
+
+    class Meta:
+        verbose_name = 'пользователь'
+        verbose_name_plural = 'пользователи'
+
+    def __str__(self):
+        return self.get_full_name()

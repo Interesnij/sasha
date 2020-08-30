0439170ede0124205eeadd33471c233a1105b3d5
Serg <ochkarik1983@mail.ru@>
2020-08-30 18:49:52 +0300

    H

--- movies/views.py
+++ movies/views.py
@@ -7,6 +7,7 @@ from django.http import HttpResponse, HttpResponseBadRequest
 from django.shortcuts import render
 from django.views import View
 from django.http import Http404
+from django.views.generic import ListView
 
 
 class MovieDetailView(TemplateView):

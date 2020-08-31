e550a34cb414673b7ebc53525ba6d311887075b4
Serg <ochkarik1983@mail.ru@>
2020-08-31 15:37:07 +0300

    J

---
+++ delete.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from locale import *
+import csv,sys,os
+
+project_dir = '../tr/tr/'
+
+sys.path.append(project_dir)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+
+import django, json, requests
+
+django.setup()
+
+from django.conf import settings
+from users.models import User
+
+User.objects.all().delete()

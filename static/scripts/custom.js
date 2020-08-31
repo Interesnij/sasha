6a5351e9f8da7d74b9532f50acb02adb942a1421
Serg <ochkarik1983@mail.ru@>
2020-08-31 19:42:48 +0300

    J

--- static/scripts/custom.js
+++ static/scripts/custom.js
@@ -306,6 +306,9 @@
 
   })(jQuery);
 
+
+  function on(elSelector,eventName,selector,fn) {var element = document.querySelector(elSelector);element.addEventListener(eventName, function(event) {var possibleTargets = element.querySelectorAll(selector);var target = event.target;for (var i = 0, l = possibleTargets.length; i < l; i++) {var el = target;var p = possibleTargets[i];while(el && el !== element) {if (el === p) {return fn.call(p, event);}el = el.parentNode;}}});};
+
   on('body', 'click', '#register_ajax', function() {
     if (!document.body.querySelector("#id_username").value){
       document.body.querySelector("#id_first_name").style.border = "1px #FF0000 solid";
--- static/scripts/video.js
+++ static/scripts/video.js
@@ -87,8 +87,6 @@ function toast_warning(text){
 	toasts.showWarning(text);
 }
 
-function on(elSelector,eventName,selector,fn) {var element = document.querySelector(elSelector);element.addEventListener(eventName, function(event) {var possibleTargets = element.querySelectorAll(selector);var target = event.target;for (var i = 0, l = possibleTargets.length; i < l; i++) {var el = target;var p = possibleTargets[i];while(el && el !== element) {if (el === p) {return fn.call(p, event);}el = el.parentNode;}}});};
-
 function send_comment(form, block, link){
   form_comment = new FormData(form);
   link_ = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject( 'Microsoft.XMLHTTP' );

a37b5321247a1f54569b49c34a9bbee9c2dfa639
Serg <ochkarik1983@mail.ru@>
2020-08-31 19:03:26 +0300

    J

--- users/views.py
+++ users/views.py
@@ -2,6 +2,7 @@ from django.views.generic.base import TemplateView
 from generic.mixins import CategoryListMixin
 from users.models import User
 from django.http import Http404
+from users.forms import UserForm
 
 
 class UserView(TemplateView, CategoryListMixin):
@@ -15,3 +16,25 @@ class UserView(TemplateView, CategoryListMixin):
 		context=super(UserView,self).get_context_data(**kwargs)
 		context["user"] = self.user
 		return context
+
+
+class UserSettings(TemplateView):
+	template_name = "user_settings.html"
+	form = None
+
+	def get(self,request,*args,**kwargs):
+		self.user = User.objects.get(pk=self.kwargs["pk"])
+		return super(UserSettings,self).get(request,*args,**kwargs)
+
+	def get_context_data(self,**kwargs):
+		context = super(UserSettings,self).get_context_data(**kwargs)
+		context["form"] = self.form
+		return context
+
+	def post(self,request,*args,**kwargs):
+		self.form = UserForm(request.POST, instance=self.user)
+		if self.form.is_valid():
+			user = self.request.user
+			self.form.save()
+			return redirect('user', pk=user.pk)
+		return super(UserSettings,self).post(request,*args,**kwargs)

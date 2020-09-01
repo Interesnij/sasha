6d49e72bacb39685f7c41465ea3c84ddd40e926b
Serg <ochkarik1983@mail.ru@>
2020-09-01 15:44:18 +0300

    H

--- about/urls.py
+++ about/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import url
-from about.views import AboutView
+from about.views import AboutView, FeedbackView
 
 
 urlpatterns = [
     url(r'^$', AboutView.as_view(), name='about'),
-    url(r'send_message/^$', FeedbackFormView.as_view()),
+    url(r'send_message/^$', FeedbackView.as_view()),
 ]

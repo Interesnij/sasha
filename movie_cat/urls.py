d56ae9615ba2b2fafc0b9b0026b40a0237796c3a
Serg <ochkarik1983@mail.ru@>
2020-09-01 11:17:10 +0300

    H

--- movie_cat/urls.py
+++ movie_cat/urls.py
@@ -3,6 +3,6 @@ from movie_cat.views import MovieListView, MovieLists
 
 
 urlpatterns = [
-    url(r'^(?P<slug>[\w\-]+)/$', MovieListView.as_view(), name='movies_index'),
     url(r'^playlists/$', MovieLists.as_view(), name='movies_list'),
+    url(r'^(?P<slug>[\w\-]+)/$', MovieListView.as_view(), name='movies_index'),
 ]

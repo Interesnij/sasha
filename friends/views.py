04fb03209eb03f2f7a39eb44fc83a6135921b956
Serg <ochkarik1983@mail.ru@>
2020-09-01 14:22:34 +0300

    H

--- friends/views.py
+++ friends/views.py
@@ -1,6 +1,12 @@
-from django.views.generic.base import TemplateView
 from generic.mixins import CategoryListMixin
+from django.views.generic import ListView
+from friends.models import Friend
 
 
-class FriendsView(TemplateView, CategoryListMixin):
-    template_name = "friends.html"
+class FriendsView(ListView, CategoryListMixin):
+	model = Friend
+	template_name="friends.html"
+
+	def get_queryset(self):
+		friends = Friend.objects.only("pk")
+		return friends

f5e58b555dabf1b1edb575c321cf5283c04f9d88
Serg <ochkarik1983@mail.ru@>
2020-09-02 14:57:41 +0300

    H

--- static/scripts/custom.js
+++ static/scripts/custom.js
@@ -72,37 +72,37 @@
 	$('#hero-slider').on('mouseup touchend', function(event) {
 		$('body').removeClass('scale-up');
 	});
+  var app = function () {
+    var body = undefined;
+    var menu = undefined;
+    var menuItems = undefined;
 
+    var init = function init() {
+      body = document.querySelector('body');
+      menu = document.querySelector('.menu-icon');
+      menuItems = document.querySelectorAll('.nav__list-item');
+
+      applyListeners();
+    };
+
+    var applyListeners = function applyListeners() {
+      menu.addEventListener('click', function () {
+        return toggleClass(body, 'nav-active');
+      });
+    };
+
+    var toggleClass = function toggleClass(element, stringClass) {
+      if (element.classList.contains(stringClass)) element.classList.remove(stringClass);else element.classList.add(stringClass);
+    };
+
+    init();
+  }();
+  
+scrollBanner();
 
 	$(document).ready(function() {
-    var app = function () {
-  		var body = undefined;
-  		var menu = undefined;
-  		var menuItems = undefined;
 
-  		var init = function init() {
-  			body = document.querySelector('body');
-  			menu = document.querySelector('.menu-icon');
-  			menuItems = document.querySelectorAll('.nav__list-item');
-
-  			applyListeners();
-  		};
-
-  		var applyListeners = function applyListeners() {
-  			menu.addEventListener('click', function () {
-  				return toggleClass(body, 'nav-active');
-  			});
-  		};
-
-  		var toggleClass = function toggleClass(element, stringClass) {
-  			if (element.classList.contains(stringClass)) element.classList.remove(stringClass);else element.classList.add(stringClass);
-  		};
-
-  		init();
-  	}();
-    
     window.scrollReveal = new scrollReveal();
-    scrollBanner();
 
 		var offset = 300;
 		var duration = 400;

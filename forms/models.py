57f9bf921e65d30233dc70783bba7c9ff77119e8
Serg <ochkarik1983@mail.ru@>
2020-10-09 15:21:16 +0300

    H

--- forms/models.py
+++ forms/models.py
@@ -1,19 +1,18 @@
 from django.db import models
 from django.contrib.postgres.indexes import BrinIndex
-from django.http import HttpResponse
 
 
 class Blank(models.Model):
 	title = models.CharField(max_length=200, verbose_name="Название")
 	file = models.FileField(upload_to='uploads/%Y/%m/%d/', verbose_name="Документ")
 	created = models.DateTimeField(auto_now_add=True, auto_now=False, verbose_name="Создан")
+	order = models.PositiveIntegerField(default=0)
 
 	class Meta:
-		ordering = ["-created"]
+		ordering = ["order"]
 		verbose_name = "Документ"
 		verbose_name_plural = "Документы"
 		indexes = (BrinIndex(fields=['created']),)
-		db_table = '"Шаблоны заявлений"'
 
 	def get_blank(self):
 		filename = self.file.name.split('/')[-1]

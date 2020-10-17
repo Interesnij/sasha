17e96c828fa71977492a7a245c4e10458b83df05
Serg <ochkarik1983@mail.ru@>
2020-10-17 11:31:55 +0300

    J

--- blog_cat/urls.py
+++ blog_cat/urls.py
@@ -3,6 +3,6 @@ from blog_cat.views import BlogListView, BlogLists
 
 
 urlpatterns = [
-    url(r'^categories/$', BlogLists.as_view(), name='blog_list'),
+    url(r'^$', BlogLists.as_view(), name='blog_list'),
     url(r'^(?P<slug>[\w\-]+)/$', BlogListView.as_view(), name='blog_index'),
-] 
+]

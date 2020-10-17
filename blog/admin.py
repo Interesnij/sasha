e490bf252e384855ed4a68ad739d78f2e25cb3c7
Serg <ochkarik1983@mail.ru@>
2020-10-17 13:05:38 +0300

    J

--- blog/admin.py
+++ blog/admin.py
@@ -16,7 +16,10 @@ class BlogAdmin(admin.ModelAdmin):
         form.base_fields['creator'].initial = request.user
         return form
 
-    
+    def get_readonly_fields(self, request, obj=None):
+        if obj is None:
+            return ['creator']
+        return []
 
 class BlogCommentAdmin(admin.ModelAdmin):
     list_display = ['text','commenter','created']

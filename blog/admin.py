ccd94a33819743e81bc6fce83eedf1c5544733be
Serg <ochkarik1983@mail.ru@>
2020-10-17 12:51:24 +0300

    J

--- blog/admin.py
+++ blog/admin.py
@@ -6,13 +6,19 @@ class BlogAdmin(admin.ModelAdmin):
     list_display = ['title', 'description', 'created']
     list_filter = ['created', 'category']
     search_fields = ['title', 'description', 'created']
+    exclude = ('creator', 'count')
+
     class Meta:
             model = Blog
+
+    def get_changeform_initial_data(self, request):
+        return {'creator': request.user}
 
 class BlogCommentAdmin(admin.ModelAdmin):
     list_display = ['text','commenter','created']
     list_filter = ['created']
     search_fields = ['created','text','commenter']
+
     class Meta:
             model = BlogComment
 

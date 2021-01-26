e6de909c54d68f275265430034c6d83d8baa576d
Serg <ochkarik1983@mail.ru@>
2021-01-26 18:45:17 +0300

    J

--- forms/models.py
+++ forms/models.py
@@ -2,11 +2,27 @@ from django.db import models
 from django.contrib.postgres.indexes import BrinIndex
 
 
+class BlankCategory(models.Model):
+	name = models.CharField(max_length=100, verbose_name="Название")
+	slug = models.CharField(max_length=100, verbose_name="Англ. название для ссылки")
+	order = models.PositiveSmallIntegerField(default=0, verbose_name="Порядковый номер")
+	def __str__(self):
+		return self.name
+	class Meta:
+		ordering = ["order", "name"]
+		verbose_name = "категория блога"
+		verbose_name_plural = "категории блога"
+
+	def __str__(self):
+		return self.name
+
+
 class Blank(models.Model):
 	title = models.CharField(max_length=200, verbose_name="Название")
 	file = models.FileField(upload_to='uploads/%Y/%m/%d/', verbose_name="Документ")
 	created = models.DateTimeField(auto_now_add=True, auto_now=False, verbose_name="Создан")
 	order = models.PositiveIntegerField(default=0)
+	category = models.ManyToManyField(BlankCategory, blank=True, null=True, verbose_name="Категория")
 
 	class Meta:
 		ordering = ["order"]

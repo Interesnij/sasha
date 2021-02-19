26e0e583a467ed7425ece691408767e49235ed3f
Serg <ochkarik1983@mail.ru@>
2021-02-19 19:36:37 +0300

    J

--- sasha/storage.py
+++ sasha/storage.py
@@ -4,5 +4,5 @@ from django.core.files.storage import FileSystemStorage
 
 class ASCIIFileSystemStorage(FileSystemStorage):
     def get_valid_name(self, name):
-        name = unicodedata.normalize('NFKD', name).encode('ascii', 'ignore')
+        name = unicodedata.normalize('NFKD', name).encode('utf8') 
         return super(ASCIIFileSystemStorage, self).get_valid_name(name)
